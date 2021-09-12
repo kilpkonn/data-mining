598e882f68f7dc29244f7d960961d6b671c93d08
Tavo Annus <tavo.annus@gmail.com>
2021-09-12 20:32:19 +0300

    Generic grid wip

--- dbscan.R
+++ dbscan.R
@@ -1,22 +1,3 @@
-library(shotGroups)
-
-rm(list = ls())
-
-source("distance-functions.R")
-
-# Load the data, note that variable "data_matrix" will appear!!
-load(file="./data/gaussians.RData")
-
-sample_size <- floor(0.7 * nrow(data_matrix))
-
-# Set seed to make results reproducible
-# set.seed(123)
-
-train_ind <- sample(seq_len(nrow(data_matrix)), size = sample_size)
-
-train_set <- data_matrix[train_ind, ]
-test_set <- data_matrix[-train_ind, ]
-
 dbscan <- function(data, s, n, r) {
   lbls <- matrix(nrow = nrow(data), ncol = 1)
   lbl <- 1
@@ -73,26 +54,4 @@ dbscan <- function(data, s, n, r) {
   }
   
   return(lbls)
-}
-
-dist_fn <- function(a, b) {
-  res <- minkowsky_dist(a, b, 2)
-  return(res)
-}
-
-lbls = dbscan(train_set, dist_fn, 6, 0.48)
-
-for (i in 1:nrow(test_set)) {
-  color <- switch(lbls[[i]],"red","green","blue","orange","magenta")
-  a <- matrix(train_set[i,], nrow = 1, ncol = ncol(train_set))
-  plot(a, col=color, type="p", xlim=c(-10,20), ylim=c(-10,20))
-  par(new=TRUE)
-}
-
-for (i in 1:nrow(test_set)) {
-  if (is.na(lbls[[i, 1]])) {
-    a <- matrix(train_set[i,], nrow = 1, ncol = ncol(train_set))
-    plot(a, col="black", type="p", xlim=c(-10,20), ylim=c(-10,20))
-    par(new=TRUE)
-  }
 }
